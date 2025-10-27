7939099a799066ae12155e50fb2f7515d5ae8885
naglis mockevicius <48673743+naglismoc@users.noreply.github.com>
2025-10-27 19:55:00 +0200

    minor fix in c task

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ arr = [4,6,48,468,68,468,648,20,20,48,30,32,20]
 for i in arr:
     print(i)
 
-print("labas rytas")
-print("labas rytas")
-print("labas rytas")
+print("labas rytas!")
+print("labas rytas!")
+print("labas rytas!")
